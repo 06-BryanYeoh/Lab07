306c53e0f59832009e580679b498976ddccad235
06-BryanYeoh <bryanyeoh1248@gmail.com>
2022-10-26 11:15:39 +0800

    Updated Player Script
    Duck can now fly

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private float duckflyspeed = 4f;
+    private Rigidbody ducky;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        ducky = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            ducky.velocity = ducky.velocity + Vector3.up * duckflyspeed;
+        }
     }
 }
